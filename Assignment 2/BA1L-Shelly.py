bbea00c487c0b402b611abd0c18a8d1443d0dd2c
shelly1804 <39118798+shelly1804@users.noreply.github.com>
2018-05-24 18:16:46 +0200

    Add files via upload

---
+++ Assignment 2/BA1L-Shelly.py
@@ -0,0 +1,17 @@
+def PatternToNumber(pattern):
+    if len(pattern)==0:
+        return 0
+    return 4 * PatternToNumber(pattern[0:-1]) + symbolToNumber(pattern[-1:])
+def symbolToNumber(symbol):
+    if symbol == "A":
+        return 0
+    if symbol == "C":
+        return 1
+    if symbol == "G":
+        return 2
+    if symbol =="T":
+        return 3
+print(PatternToNumber("AGT"))
+
+
+
